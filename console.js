d5eb3653fee2356351ae07e113874468f3a828e9
rfcho322 <98252280+rfcho322@users.noreply.github.com>
2023-02-01 05:26:31 +0800

    Update console.js

--- console.js
+++ console.js
@@ -3,3 +3,11 @@ function testCommit () {
 }
 
 console.log(testCommit)
+
+//Session Test
+
+if(!empty($sessData['status']['msg'])){ 
+    $statusMsg = $sessData['status']['msg']; 
+    $statusMsgType = $sessData['status']['type']; 
+    unset($_SESSION['sessData']['status']); 
+}
